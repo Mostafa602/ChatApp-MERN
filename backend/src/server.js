bc62680f80675ee6be38c13ebba4e67812d8e3a5
Mostafa Mohamed <mostafamohamed62005@gmail.com>
2026-03-19 16:39:46 +0200

    Added start script at the backend file

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import userRoutes from "./routes/user.routes.js"
 import connectDB from "./config/db.js"
 import cookieParser from 'cookie-parser'
 import protectRoute from "./middleware/protectRoute.js"
-import {app, server} from "./socket/socket.js"
+import { app, server } from "./socket/socket.js"
 import cors from "cors"
 import path from "path"
 
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 if (process.env.NODE_ENV !== "production") {
-        app.use(cors({
+    app.use(cors({
         origin: "http://localhost:3000",
         credentials: true,
     }))
@@ -36,12 +36,12 @@ app.use("/api/users", protectRoute, userRoutes)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+    app.get(/(.*)/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
 
-connectDB().then( () => {
+connectDB().then(() => {
     server.listen(PORT, () => {
         console.log(`App started listening on PORT ${PORT}`);
     })
